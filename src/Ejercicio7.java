c145a7d7545ab6e0b13ae37a57d7b4924f807ae2
CrisMesina <cmesinamorales@gmail.com>
2026-08-29 04:04:36 -0400

    ejer8 XD

---
+++ src/Ejercicio7.java
@@ -0,0 +1,16 @@
+public class Ejercicio7 {
+
+    public static void main(String[] args){
+
+        int edad = 25;
+        boolean tieneEntrada = true;
+
+        if (edad >= 18 && tieneEntrada){
+            System.out.println("Acceso permitido");
+        }else{
+            System.out.println("Acceso Denegado");
+        }
+
+    }
+
+}
---
+++ src/Ejercicio8.java
@@ -0,0 +1,17 @@
+public class Ejercicio8 {
+
+    public static void main(String[] args){
+
+
+        boolean tieneMembresia = false;
+        boolean tieneCupon = true;
+
+        if (tieneMembresia || tieneCupon){
+            System.out.println("Descuento aplicado");
+        }else{
+            System.out.println("No tienes descuento");
+        }
+
+    }
+
+}
