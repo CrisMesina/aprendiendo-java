7d1930e3b78d85eaab2d574aa21e6f214924677a
CrisMesina <cmesinamorales@gmail.com>
2026-08-29 01:25:58 -0400

    Subir proyecto

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre = "Cristopher";
+        int edad = 17;
+        boolean estudios = true;
+
+
+        if (edad >= 18 && estudios){
+            System.out.println("Eres mayor de edad y tienes estudios");
+        }else {
+            System.out.println("ALo");
+        }
+
+
+
+    }
+}
